501eb01d0b9ae5f6debdcfef43c458d63b16bb9c
Karthik Sivadas <karthiksivadas@outlook.com>
2024-07-27 19:03:43 +0530

    feat: Pwoli metrics

---
+++ platform/app/models/user.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class User < ApplicationRecord
+  # Include default devise modules.
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable
+
+  include DeviseTokenAuth::Concerns::User
+end
---
+++ platform/config/initializers/cors.rb
@@ -0,0 +1,9 @@
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins '*'
+    resource '*',
+      headers: :any,
+      expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+      methods: [:get, :post, :options, :delete, :put]
+  end
+end
