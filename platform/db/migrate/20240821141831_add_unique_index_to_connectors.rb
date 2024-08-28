c45f267fd99bbdd198eb5e006b3efbca1dd4de3a
Karthik Sivadas <karthiksivadas@outlook.com>
2024-08-29 00:45:10 +0530

    feat: Add docker-compose for just rails (#3)
    * feat: Add docker-compose for just rails
    * fix: Update failing github action
    * fix: rubocop errors
    * fix: gtihub action rspec failure

--- platform/db/migrate/20240821141831_add_unique_index_to_connectors.rb
+++ platform/db/migrate/20240821141831_add_unique_index_to_connectors.rb
@@ -5,6 +5,7 @@ class AddUniqueIndexToConnectors < ActiveRecord::Migration[7.1]
 
   def change
     add_index :connectors, %i[workspace_id name configuration], unique: true,
-                                                                name: "index_connectors_on_workspace_name_config", algorithm: :concurrently
+                                                                name: "index_connectors_on_workspace_name_config",
+                                                                algorithm: :concurrently
   end
 end
