beb91367abcfb3f91f805b7856edc1c3f1c48c12
Karthik Sivadas <karthiksivadas@outlook.com>
2024-09-10 20:50:55 +0530

    feat: Add ruby connector to rails docker to fetch connection specification (#18)

--- ruby_connectors/spec/ruby_connectors_spec.rb
+++ ruby_connectors/spec/ruby_connectors_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe RubyConnectors do
   it "has a version number" do
     expect(RubyConnectors::VERSION).not_to be_nil
   end
-
-  it "connects to the database" do
-    expect(RubyConnectors::DB).to be_a(Sequel::Database)
-  end
 end
