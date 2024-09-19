fdd646502723cfdb02b18902079816fb85d61b33
Abhishek Panicker <abhishek10281@gmail.com>
2024-09-19 21:35:15 +0530

    feat: updated  services and connector Form
    1. Created Connector Form
    2. Updated Services Structure.
    3. Added Payload Type file for connectors

--- ui/src/routes/_authenticated/connectors/add-connector/index.tsx
+++ ui/src/routes/_authenticated/connectors/add-connector/index.tsx
@@ -104,10 +104,13 @@ function AddConnector() {
                         </Button>
                     </div>
                     <div className="flex h-20 py-5 px-10 justify-between w-full text-sm font-medium tracking-normal border-t border-solid border-b-slate-200 text-slate-400">
-                        <Button className={`${steps  === 0 ? 'hidden' : ''}`} onClick={()=>onPrev()}>
+                        <Button
+                            className={`${steps === 0 ? 'hidden' : ''}`}
+                            onClick={() => onPrev()}
+                        >
                             Back
                         </Button>
-                        <Button  className="ml-auto" onClick={()=>onNext()}>
+                        <Button className="ml-auto" onClick={() => onNext()}>
                             Next
                         </Button>
                     </div>
