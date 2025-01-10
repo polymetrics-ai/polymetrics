2767f808adb51c48703454a37f48bcbac4271a4a
Karthik Sivadas <karthiksivadas@outlook.com>
2025-01-10 20:28:36 +0530

    fix: Refactor redirect logic when user is not authenticated

--- ui/src/routes/index.tsx
+++ ui/src/routes/index.tsx
@@ -1,13 +1,17 @@
 import { createFileRoute, redirect } from '@tanstack/react-router';
-import useAuthStore from '@/store/authStore'; // Removed AuthContext as it's unused
+import useAuthStore from '@/store/authStore';
 
 export const Route = createFileRoute('/')({
     beforeLoad: async () => {
         const { isAuthenticated } = useAuthStore.getState();
-        console.log(isAuthenticated);
+        
         if (isAuthenticated) {
             throw redirect({
                 to: '/dashboard'
+            });
+        } else {
+            throw redirect({
+                to: '/login'
             });
         }
     }
